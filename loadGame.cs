c4302159a7111e9af6c56adb2fe8031e4efdcbe9
RonanMichael <44853197+RonanMichael@users.noreply.github.com>
2022-05-15 18:07:56 +0100

    Add files via upload

---
+++ loadGame.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class loadGame : MonoBehaviour
+{
+    private sceneHandler sceneHandler;
+
+    void Awake()
+    {
+        sceneHandler = GameObject.FindObjectOfType<sceneHandler>();
+
+    }
+
+    public void loadScene()
+    {
+
+            Debug.Log("Load Successful");
+            sceneHandler.LoadScene();
+
+    }
+}
